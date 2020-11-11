0dbba1d3dd058cab8760150acf5059c541b1d163
Advik-20 <73420459+Advik-20@users.noreply.github.com>
2020-11-11 21:47:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var bullet,wall,thickness;
+var speed,weight;
+
+function setup() {
+  createCanvas(1600,400);
+  
+  speed = random(223,321);
+  weight = random(30,52);
+  thickness = random(22,83);
+
+  bullet = createSprite(50,200,50,10);
+  bullet.velocityX = speed;
+  bullet.shapeColor = "white";
+
+  wall = createSprite(1500,200,thickness,height/2);
+  wall.shapeColor = "white";
+}
+
+function draw() {
+  background("black");  
+
+  if (hasCollided(bullet,wall)){
+    bullet.velocityX = 0;
+    var damage = 0.5 * weight * speed * speed/(thickness * thickness * thickness);
+    
+    if (damage > 10){
+      wall.shapeColor = "red";
+    }
+
+    if (damage < 10){
+      wall.shapeColor = "green";
+    }
+  }
+
+  drawSprites();
+}
+
+function hasCollided(lBullet,lWall){
+  var bulletRightEdge = lBullet.x + lBullet.width
+  var wallLeftEdge = lWall.x;
+  if (bulletRightEdge >=wallLeftEdge){
+    return true;
+  }
+
+  return false;
+}
